dc9442a2f20d6458e5a286c34d7640bdc286cd39
Ali Sloyan <alisloyan@MacBook-Pro-Ali.local>
2019-05-28 15:20:08 +0300

    Configured BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,35 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function() {
     return gulp.src('src/styles/index.scss')
     .pipe(sass())
     .pipe(gulp.dest('dist/styles'))
 });
+
+gulp.task('copy-img', function() {
+    return gulp
+    .src('./images/*')
+    .pipe(gulp.dest('./dist/images'));
+});
+
+gulp.task('copy-fonts', function() {
+    return gulp
+    .src('./src/fonts/*/*')
+    .pipe(gulp.dest('./dist/fonts'));
+})
+
+gulp.task('watch', gulp.series(['sass', 'copy-img', 'copy-fonts'], function (done) {
+    browserSync.reload();
+    done();
+}));
+
+gulp.task('default', gulp.series(['sass', 'copy-img', 'copy-fonts'], function () {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+    gulp.watch('src/styles/**/*.scss', gulp.series(['watch']));
+}));
