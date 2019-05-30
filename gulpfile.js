65b91598151ba75fe6a3903b3082414c7198bde3
Ali Sloyan <alisloyan@MacBook-Pro-Ali.local>
2019-05-30 11:10:34 +0300

    Add Feedback block

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,38 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var browserSync = require("browser-sync").create();
 
-gulp.task('sass', function() {
-    return gulp.src('src/styles/index.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('dist/styles'))
+gulp.task("sass", function() {
+  return gulp
+    .src("src/styles/index.scss")
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(gulp.dest("dist/styles"));
 });
 
-gulp.task('copy-img', function() {
-    return gulp
-    .src('./images/*')
-    .pipe(gulp.dest('./dist/images'));
+gulp.task("copy-img", function() {
+  return gulp.src("./images/*").pipe(gulp.dest("./dist/images"));
 });
 
-gulp.task('copy-fonts', function() {
-    return gulp
-    .src('./src/fonts/*/*')
-    .pipe(gulp.dest('./dist/fonts'));
-})
+gulp.task("copy-fonts", function() {
+  return gulp.src("./src/fonts/*/*").pipe(gulp.dest("./dist/fonts"));
+});
 
-gulp.task('watch', gulp.series(['sass', 'copy-img', 'copy-fonts'], function (done) {
+gulp.task(
+  "watch",
+  gulp.series(["sass", "copy-img", "copy-fonts"], function(done) {
     browserSync.reload();
     done();
-}));
+  })
+);
 
-gulp.task('default', gulp.series(['sass', 'copy-img', 'copy-fonts'], function () {
+gulp.task(
+  "default",
+  gulp.series(["sass", "copy-img", "copy-fonts"], function() {
     browserSync.init({
-        server: {
-            baseDir: "./dist"
-        }
+      server: {
+        baseDir: "./dist"
+      }
     });
-    gulp.watch('src/styles/**/*.scss', gulp.series(['watch']));
-}));
+    gulp.watch("src/styles/**/*.scss", gulp.series(["watch"]));
+  })
+);
